bcb63dfeb5e045bd86e7d46f1ba350499b59c451
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-11-04 06:43:38 +0300

    fixing build error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { DessertsComponent } from './desserts/desserts.component';
 
 export const routes: Routes = [
-  {path: '', component: DessertsComponent, title: 'Desserts'}
+    {path: '', component: DessertsComponent, title: 'Desserts'}
 ];
